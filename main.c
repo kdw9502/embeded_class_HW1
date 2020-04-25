182e3ea4a7c434f37de811bfff9e5e06f414cc62
kdw9502 <kdw9502@gmail.com>
2020-04-25 16:54:08 +0900

    no message

--- main.c
+++ main.c
@@ -106,13 +106,14 @@ void read_hw_key() {
     struct input_event ev[BUFF_SIZE];
     int rd, size = sizeof (struct input_event);
     mode = (int*)shmat(mode_mid, (int*) NULL,0);
-
+    printf("read_hw0\n");
     if ((rd = read(hw_button_device, ev, size * BUFF_SIZE)) < size) {
 #ifdef DEBUG
         printf("hw wait return: %d\n",rd);
 #endif
         return;
     }
+    printf("read_hw2\n");
     if (ev[0].type == 1 && ev[0].value == 1) // on key press
     {
         if (ev[0].code == BACK_KEY_CODE) {
