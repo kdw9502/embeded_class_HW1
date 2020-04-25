3dfb1bed3d7c77360b57555c6821b0623ecc3ce4
kdw9502 <kdw9502@gmail.com>
2020-04-25 18:52:25 +0900

    no message

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 void input_process() {
+    printf("init input process");
     if ((fpga_switch_device = open("/dev/fpga_push_switch", O_RDWR| O_NONBLOCK)) == -1) {
         printf("Switch Device Open Error\n");
         return;
@@ -17,7 +18,7 @@ void input_process() {
 }
 
 void main_process() {
-    printf("main 1");
+    printf("init main process");
     int *mode_addr;
     while (1) {
         printf("main 2");
@@ -45,6 +46,7 @@ void main_process() {
 }
 
 void output_process() {
+    printf("init output process");
     int *mode_addr;
 
     if ((fpga_fnd_device = open("/dev/fpga_fnd", O_RDWR)) == -1)
